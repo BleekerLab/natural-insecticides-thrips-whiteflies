1ad00802ae54e17d00dba8ee986cde3d0a7b764d
Marc Galland <m.galland@uva.nl>
2018-10-01 11:04:22 +0100

    Added regression models and goodness of fits

--- Figure7_F2_whitefly_survival/F2_wf_survival.R
+++ Figure7_F2_whitefly_survival/F2_wf_survival.R
@@ -3,6 +3,10 @@ library(reshape2)
 library(ggplot2)
 library(dplyr)
 library(RColorBrewer)
+library(lme4)
+
+# load .Rprofile file
+source("./.custom_R_profile")
 
 # where to save results?
 resdir = file.path(getwd(),"Figure7_F2_whitefly_survival/")
@@ -32,7 +36,7 @@ df$line = factor(df$line,levels = plant_ranking)
 g <- ggplot(data = df,aes(x=line,y=percentage,fill=line)) + 
   geom_boxplot() +
   geom_point() +
-  geom_jitter(width = 0,height = 0) +
+  geom_jitter(width = 0,height = 1) +
   guides(fill=FALSE) +
   scale_fill_manual(values = colorPalette) +
   scale_y_continuous(limits = c(0,100)) +
@@ -45,3 +49,39 @@ print(g)
 # save plot
 ggsave(filename = file.path(resdir,"Figure7A.png"),plot = g,width=10,height=5,dpi = 400)
 ggsave(filename = file.path(resdir,"Figure7A.svg"),plot = g,width=10,height=5)
+
+######### Logistic regressions (Generalized Linear Models and Generalized Linear Mixed Models) ##########
+# remove missing values
+# convert cage to factor
+# Calculate a probability to die for each observation
+df = na.omit(df)
+df$cage = as.factor(df$cage)
+df$line = as.factor(df$line)
+
+# add total column
+df = df %>% 
+  mutate(total=dead+alive)
+
+# Fit models
+# dead / total - dead
+fit1 = glm(cbind(dead,total-dead) ~ 1,data = df,family = binomial(link = logit)) # intercept only model
+fit2 = glm(cbind(dead,total-dead) ~ 1 + line, data=df, family = binomial(link = logit)) # model with accession
+fit3 = glmer(cbind(dead,total-dead) ~ 1 + line + (1|cage),data = df,family=binomial(link = logit)) # random plant effects
+
+# AIC criteria and goodness of fit of the models
+fits_no_mixed = list(fit1,fit2)
+fit_mixed = list(fit3)
+
+AICs = cbind.data.frame(
+  models = sapply(c(fits_no_mixed,fits_mixed),function(x) {deparse(formula(x))}),
+  AIC = sapply(c(fits_no_mixed,fits_mixed),function(x) AIC(x)),
+  dAIC = as.vector(bbmle::AICtab(fit1,fit2,fit3)$dAIC)
+)
+AICs$models = c("model1","model2","model3")
+
+plotAIC = ggplot(AICs,aes(models,AIC)) + 
+  geom_bar(stat="identity") + 
+  theme(axis.text.x = element_text(angle=45,hjust=1)) +
+  ggtitle("AIC for each model")
+print(plotAIC)
+
