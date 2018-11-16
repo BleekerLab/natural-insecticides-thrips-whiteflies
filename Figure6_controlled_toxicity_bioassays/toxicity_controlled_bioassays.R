b5ff401fb9ba13a960eb24924309f1088922920f
mgalland <m.galland@uva.nl>
2018-11-16 09:49:26 +0100

    updated Figure6A (wf) and corresponding datasets

--- Figure6_controlled_toxicity_bioassays/toxicity_controlled_bioassays.R
+++ Figure6_controlled_toxicity_bioassays/toxicity_controlled_bioassays.R
@@ -25,14 +25,22 @@ wf$percentage = with(wf,alive / (alive + dead) * 100)
 
 wf$dose = factor(x = wf$dose,levels = unique(wf$dose))
 
-p1 <- ggplot(wf,mapping = aes(x = dose,y = percentage,fill=compound)) +
+p <- ggplot(wf,mapping = aes(x = dose,y = percentage,fill=compound)) +
   geom_boxplot() +
   geom_jitter(width = 0.1) +
-  facet_grid(cols = vars(compound)) +
+  facet_wrap(. ~ compound,scales="free_x",nrow = 2) +
   labs(x = "Dose",y="Whitefly survival after 48h (%)") +
-  scale_fill_brewer(palette = "OrRd")
-p1
+  scale_fill_brewer(palette = "Set1") +
+  theme(axis.text = element_text(colour = "black",size = 8),axis.text.x = element_text(angle = 0))
+p
 
+svg(filename = "Figure6_controlled_toxicity_bioassays/Figure6A.svg",width = 10,height = 5)
+print(p)
+dev.off()
+
+pdf(file = "Figure6_controlled_toxicity_bioassays/Figure6A.pdf",width = 10,height = 5)
+print(p)
+dev.off()
 
 ##############################################################
 # Survival curves for thrips survival (continous measurements)
@@ -64,7 +72,12 @@ for (i in seq_along(compounds)){
                  conf.int=TRUE,
                  ggtheme = theme_bw()
                  ) 
-  g <- p$plot + theme_bw() + facet_grid(~strata)
+  p <- ggpar(p,legend = "none")
+  g <- p$plot + 
+    theme_bw() + 
+    ggtitle(compounds[[i]]) +
+    facet_wrap(~strata,nrow = 1) +
+    theme(axis.title.y = element_blank(),legend.position = "none")
   list_of_plots[[i]]=g
 }
 
