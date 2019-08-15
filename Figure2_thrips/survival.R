e4b8679691c0550dfebd52d813f91b5a31b15d1d
Marc Galland <m.galland@uva.nl>
2019-08-15 11:16:18 +0200

    fixed legend issue in Fig2B

--- Figure2_thrips/survival.R
+++ Figure2_thrips/survival.R
@@ -68,24 +68,24 @@ df.medians$accession = factor(df.medians$accession,levels = df.medians$accession
 ##################################
 # Plot barplot of median survivals
 ##################################
-p <- ggplot(data=df.medians,aes(x=accession,y=median,fill=color)) +
+p <- ggplot(data=df.medians,aes(x=accession,y=median,fill=species)) +
   geom_bar(stat="identity",color="black") +
   theme(axis.text.x = element_text(angle=90))
-p
+
+ggsave(filename = "Figure2_thrips/Figure2B.pdf",plot = p,width = 7,height = 5)
 
 ######################
 # Plot survival curves
 ######################
 
-# reorder by increasing survival time
-df$genotype = factor(x = df$genotype,levels = df$genotype)
-
-
 # add species information
 colnames(df)[ncol(df)]="accession"
 df = dplyr::left_join(df,accession2species,by="accession")
 
+# reorder by increasing survival time
+df$genotype = factor(x = df$genotype,levels = df$genotype)
 
+# plot
 ggplot(data = df, aes(x = time, y = surv, color = color)) +
   geom_line() + 
   # plot censor marks
