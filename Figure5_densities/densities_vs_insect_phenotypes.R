0428c2f5e451d3a7665d79fe2e67ff361ab716fc
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2019-09-10 14:29:46 +0200

    eddit plot sizes

--- Figure5_densities/densities_vs_insect_phenotypes.R
+++ Figure5_densities/densities_vs_insect_phenotypes.R
@@ -3,6 +3,16 @@ library(dplyr)
 library(ggrepel)
 library(ggplot2)
 
+#############################
+# Custom theme for plotting #
+#############################
+
+my.theme = theme(axis.text.x = element_text(color = "black", size = 6),
+                 axis.text.y = element_text(color = "black", size = 6),
+                 axis.title.x = element_text(color = "black", size = 8),
+                 axis.title.y = element_text(color = "black", size = 8)
+)
+
 #Load Trichome density data + Whitefly / Thrips ranked phenotypes
 density = read.delim("Figure5_densities/trichome.counts.processed.tsv", header =T)
 pheno = read.delim("Figure3_scatterplot/data4scatterplot.txt", header = T)
@@ -24,42 +34,36 @@ density.pheno$type = factor(density.pheno$type, levels = c("non.glandular", "typ
 p.whitelfies = 
 density.pheno %>%
 ggplot() +
-  geom_point(aes(x = wf, y = density),fill="grey",color="black",shape=21,size=3) +
+  geom_point(aes(x = wf, y = density),fill="grey",color="black",shape=21,size=2) +
   theme_bw() +
   geom_label_repel(aes(x = wf, y=density,label=accession, fill=species),
+                   size = 2,
                    label.size = 0.05,
                    label.padding = 0.1,
                    show.legend = FALSE) +
-  geom_smooth(aes(x = wf, y = density),
-              method = lm,
-              alpha = 0.25,
-              linetype = "dotted",
-              size = 1)+
   facet_wrap(~type, scale = "free", ncol = 1)+
   labs(x = "Tomato genotype rank for whitefly survival (low to high survival)",y = "Trichome density (trichomes / mm2 leaf)") +
-  scale_x_continuous(breaks=seq(0,19,1))
+  scale_x_continuous(breaks=seq(0,19,1))+
+  my.theme
 
 # Plot Thrips vs. Trichome densities
 p.thrips = 
   density.pheno %>%
   ggplot() +
-  geom_point(aes(x = thrips, y = density),fill="grey",color="black",shape=21,size=3) +
+  geom_point(aes(x = thrips, y = density),fill="grey",color="black",shape=21,size=2) +
   theme_bw() +
   geom_label_repel(aes(x = thrips, y=density,label=accession, fill=species),
+                   size = 2,
                    label.size = 0.05,
                    label.padding = 0.1,
                    show.legend = FALSE) +
-  geom_smooth(aes(x = thrips, y = density),
-              method = lm,
-              alpha = 0.25,
-              linetype = "dotted",
-              size = 1)+
   facet_wrap(~type, scale = "free", ncol = 1)+
   labs(x = "Tomato genotype rank for thrips survival (low to high survival)",y = "Trichome density (trichomes / mm2 leaf)") +
-  scale_x_continuous(breaks=seq(0,19,1))
+  scale_x_continuous(breaks=seq(0,19,1))+
+  my.theme
 
 ##############
 # SAVE PLOTS #
 ##############
-ggsave(filename = file.path("Figure5_densities/plots/WF_vs_densities.pdf"),plot = p.whitelfies, width = 12,height = 25, units = "cm")
-ggsave(filename = file.path("Figure5_densities/plots/Thrips_vs_densities.pdf"),plot = p.thrips, width = 12,height = 25, units = "cm")
+ggsave(filename = file.path("Figure5_densities/plots/WF_vs_densities.pdf"),plot = p.whitelfies, width = 9,height = 21, units = "cm")
+ggsave(filename = file.path("Figure5_densities/plots/Thrips_vs_densities.pdf"),plot = p.thrips, width = 9,height = 21, units = "cm")
