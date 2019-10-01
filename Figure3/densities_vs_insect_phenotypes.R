5c82b92be0ba79efc7dd2b0aca8567c0a1220adb
Marc Galland <m.galland@uva.nl>
2019-10-01 17:31:47 +0200

    modify script and add figures

--- Figure3/densities_vs_insect_phenotypes.R
+++ Figure3/densities_vs_insect_phenotypes.R
@@ -1,7 +1,7 @@
 library(RColorBrewer)
-library(dplyr)
 library(ggrepel)
-library(ggplot2)
+library(tidyverse)
+library(Rmisc)
 
 #############################
 # Custom theme for plotting #
@@ -13,24 +13,37 @@ my.theme = theme(axis.text.x = element_text(color = "black", size = 6),
                  axis.title.y = element_text(color = "black", size = 8)
 )
 
-#Load Trichome density data + Whitefly / Thrips ranked phenotypes
-density = read.delim("Figure5_densities/trichome.counts.processed.tsv", header =T)
-pheno = read.delim("Figure3_scatterplot/data4scatterplot.txt", header = T)
+##################################################
+# Load datasets
+##################################################
+
+# trichome density data
+density = read.delim("FigureS4/trichome.counts.processed.tsv", header =T)
+
+# whitefly and thrips ranks based on toxicity
+pheno = read.delim("Figure2/data4scatterplot.txt", header = T)
 pheno = separate(pheno, col = sample, into = c("x", "y", "accession")) %>% #separate "sample" into accession names only so it can be used for fusing the datasets
   select(., c("accession", "wf", "thrips"))
 
-#Calculate the average trichome densities per genotype (abaxial and adaxial are taken together)
-density.avg = summarySE(density, measurevar = "density", groupvars = c("genotype", "accession", "species" ,"type", "color"))
+################################################################################################
+# Calculate the average trichome densities per genotype (abaxial and adaxial are taken together)
+density.avg = summarySE(
+  density, 
+  measurevar = "density", 
+  groupvars = c("genotype", "accession", "species" ,"type", "color")
+  )
 
-#Fuse the two datasets to a new one
+#####################################
+# merge the two datasets to a new one
+#####################################
 density.pheno  = left_join(density.avg, pheno, by = "accession")
 density.pheno$accession = as.factor(density.pheno$accession)
 
-#Label the trichome classes nicely
+# Label the trichome classes nicely
 density.pheno$type = factor(density.pheno$type, levels = c("non.glandular", "typeIandIV", "typeVI"),
                             labels = c("Non glandular", "Type I/IV", "Type VI"))
 
-# Plot Whiteflies vs. Trichome densities
+# Plot whiteflies vs. trichome densities
 p.whitelfies = 
 density.pheno %>%
 ggplot() +
@@ -46,7 +59,7 @@ ggplot() +
   scale_x_continuous(breaks=seq(0,19,1))+
   my.theme
 
-# Plot Thrips vs. Trichome densities
+# Plot thrips vs. trichome densities
 p.thrips = 
   density.pheno %>%
   ggplot() +
@@ -62,8 +75,13 @@ p.thrips =
   scale_x_continuous(breaks=seq(0,19,1))+
   my.theme
 
-##############
-# SAVE PLOTS #
-##############
-ggsave(filename = file.path("Figure5_densities/plots/WF_vs_densities.pdf"),plot = p.whitelfies, width = 9,height = 21, units = "cm")
-ggsave(filename = file.path("Figure5_densities/plots/Thrips_vs_densities.pdf"),plot = p.thrips, width = 9,height = 21, units = "cm")
+################
+# Save the plots
+################
+ggsave(filename = file.path("Figure3/whitefly_toxicity_vs_trichome_densities.pdf"),plot = p.whitelfies, width = 9,height = 21, units = "cm")
+ggsave(filename = file.path("Figure3/thrips_toxicity_vs_trichome_densities.pdf"),plot = p.thrips, width = 9,height = 21, units = "cm")
+
+#################
+# session info
+##################
+writeLines(capture.output(sessionInfo()), "Figure3/sessionInfo.txt")
