2008bee61f104638dbc471a22ffeefa2a36af23c
mgalland <m.galland@uva.nl>
2019-11-01 11:35:52 +0100

    Change path to files

--- Figure1/Figure1A.R
+++ Figure1/Figure1A.R
@@ -3,7 +3,7 @@ suppressMessages(library(tidyverse))
 
 ############ Data import and wrangling #########
 # import whitefly no-choice data
-df = read.delim("Figure1_wf/whitefly_no-choice_19_accessions.tsv",header = T,stringsAsFactors = F)
+df = read.delim("Figure1/whitefly_no-choice_19_accessions.tsv",header = T,stringsAsFactors = F)
 
 # remove unecessary variables
 df$alive = NULL
@@ -43,7 +43,7 @@ survival <- ggplot(data = df,aes(x = accession,y = average,fill=species)) +
 print(survival)
 
 # save plot into a file
-ggsave(filename = file.path("Figure1_wf/","wf_survival_19_accessions.png"),plot = survival,width=7,height=3,dpi = 300)
-ggsave(filename = file.path("Figure1_wf/","wf_survival_19_accessions.svg"),plot = survival,width=7,height=3)
+ggsave(filename = file.path("Figure1/","wf_survival_19_accessions.png"),plot = survival,width=7,height=3,dpi = 300)
+ggsave(filename = file.path("Figure1/","wf_survival_19_accessions.svg"),plot = survival,width=7,height=3)
 
 ################################################
