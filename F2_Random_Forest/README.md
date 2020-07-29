a9dedf8e9cd843803fa0228e8bb5b9d3b356f61b
Marc Galland <m.galland@uva.nl>
2020-07-29 13:40:19 +0200

    add table and script for RF analysis

---
+++ F2_Random_Forest/README.md
@@ -0,0 +1,9 @@
+# F2 Random Forest analysis
+
+![alt text](plot1_val.png)
+
+![alt text](plot2_vip.png)
+
+![alt text](plot3_pred_vs_original.png)
+
+![alt text](plot4_perm.png)
--- F2_Random_Forest/random_forest_analysis_muvr.R
+++ F2_Random_Forest/random_forest_analysis_muvr.R
@@ -12,6 +12,7 @@ library("doParallel")
 n_cores = 3
 n_outer = 7
 n_inner = 4
+n_permutations = 100
 
 # grid search params
 start_ratio <- 0.6
@@ -30,15 +31,17 @@ registerDoParallel(cl)
 ##########
 # Input data
 ##########
-metabolites = read.delim("F2_Random_Forest/Figure8_volatiles_F2/20180625_F2metabolite_table.tsv",
+metabolites = read.delim("F2_Random_Forest/volatiles/20180625_F2metabolite_table.tsv",
                          header = T,
                          stringsAsFactors = F,
                          check.names = F)
 
 
-whitefly <- read.delim("F2_Random_Forest/Figure7_F2_whitefly_survival/whitefly_table_for_random_forest.tsv",
+whitefly <- read.delim("F2_Random_Forest/whitefly/whitefly_table_for_random_forest.tsv",
                             header = T,
-                            stringsAsFactors = F)
+                            stringsAsFactors = F) %>% 
+  group_by(line) %>% 
+  dplyr::summarise(percentage = median(percentage))
 
 df <- inner_join(metabolites, whitefly, by = "line")
 
@@ -51,14 +54,12 @@ Y = df$percentage
 
 # hyperparameter grid search --> same as above but with increased mtry values
 hyper_grid <- expand.grid(
-  ratio     = 
-    seq(from = start_ratio, 
-        to   = end_ratio, 
-        by   = step_ratio),
-  rep       = 
-    seq(from = start_nreps, 
-        to   = end_nreps, 
-        by   = step_nreps),
+  ratio     = seq(from = start_ratio, 
+                  to   = end_ratio, 
+                  by   = step_ratio),
+  rep       = seq(from = start_nreps, 
+                  to   = end_nreps, 
+                  by   = step_nreps),
   q2     = 0 # will be used to collect the Q2 metric for each RF model
 )
 
@@ -87,7 +88,7 @@ hyper_grid %>%
          rep = as.factor(rep)) %>% 
   rownames_to_column("combination") %>% 
   mutate(combination = as.numeric(combination)) %>% 
-  ggplot(., aes(combination, y = q2, color = ratio)) + 
+  ggplot(., aes(combination, y = q2, color = rep)) + 
   geom_point() + 
   scale_y_continuous(limits = c(0,1))
 
@@ -111,8 +112,64 @@ rf_model <- MUVR(X = X,
                  method = "RF", 
                  parallel = TRUE)
 
+par(mar=c(4,4,1,1))
 plotVAL(rf_model)
 plotMV(rf_model, model = "min")
+
+par(mar=c(3,10,1,1))
+plotVIP(rf_model)
+
+#####################################################
+# Compute permuted models and extract fitness metrics
+#####################################################
+
+# collect Q2 from permutations
+perm_fit = numeric(n_permutations)
+
+for (p in 1:n_permutations) {
+  cat('\nPermutation',p,'of', n_permutations)
+  YPerm = sample(Y)
+  
+  perm = MUVR(X         = X, 
+              Y         = YPerm,
+              ID =        IDs,
+              nRep      = best_params$rep,
+              nOuter    = n_outer,
+              varRatio  = best_params$ratio,
+              scale     = FALSE, 
+              DA        = FALSE, 
+              fitness   = "RMSEP", 
+              method    = "RF", 
+              parallel  = TRUE)
+  perm_fit[p] = perm$fitMetric$Q2
+}
+
+
+# actual (original RF mode)
+actual_fit <- rf_model$fitMetric$Q2[1] #(1 = "min model")
+
+# Parametric (Student’s) permutation test significance
+pvalue <- pPerm(actual = actual_fit, 
+                h0 = perm_fit,
+                side = "greater",
+                type = "t")
+
+plotPerm(actual = actual_fit, 
+         xlab = "Q2 metric",
+         h0 = perm_fit) 
+
+
+perm_fit_df = data.frame(permutation = seq(1:length(perm_fit)), q2 = perm_fit) 
+
+g <- ggplot(perm_fit_df, aes(x = q2)) + 
+  geom_histogram(bins = 10) + 
+  geom_vline(xintercept = actual_fit, col = "blue") + 
+  labs(x = "Q2 metric", y = "Frequency") +
+  ggtitle(paste("Distribution of Q2 p-values based on \n",
+                n_permutations,
+                "permutations of the Y variable \n p-value = ",
+                format(pvalue,digits = 3, decimal.mark = "."),sep = " "
+                ))
 
 stopCluster(cl)
 
