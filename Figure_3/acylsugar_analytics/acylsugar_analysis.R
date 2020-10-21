2fd3d64270faaa36ed3c245b4d6733692a24fa9f
Ruy Kortbeek <r.w.j.kortbeek@uva.nl>
2020-10-21 17:05:09 +0200

    Deeper acylsugar analysis

---
+++ Figure_3/acylsugar_analytics/acylsugar_analysis.R
@@ -0,0 +1,209 @@
+library(tidyverse)
+
+#######################
+# Load acylsugar data #
+#######################
+
+
+acylsugars = volatiles = read.csv("Figure_S6/20190904_acylsugars_peak_area_all_samples.csv", header = T, stringsAsFactors = TRUE, check.names = F)
+acylsugars.long = gather(acylsugars, 
+                         key = "metabolite",
+                         value = "abundance",
+                         -sample, -accession)
+
+#######################
+# Add metabolite info #
+#######################
+
+acylsugars.long.structure <- acylsugars.long %>%
+  filter(!grepl("unknown", metabolite, ignore.case = TRUE)) %>%
+  mutate(backbone = case_when(grepl("S", metabolite) ~ "Sucrose",
+                              grepl("G", metabolite) ~ "Glucose")) %>%
+  mutate(tails = case_when(grepl("G1", metabolite) ~ "1",
+                           grepl("G2", metabolite) ~ "2",
+                          grepl("G3", metabolite) ~ "3",
+                          grepl("G4", metabolite) ~ "4",
+                          grepl("G5", metabolite) ~ "5",
+                          grepl("G4", metabolite) ~ "4",
+                          grepl("S1", metabolite) ~ "1",
+                          grepl("S2", metabolite) ~ "2",
+                          grepl("S3", metabolite) ~ "3",
+                          grepl("S4", metabolite) ~ "4",
+                          grepl("S5", metabolite) ~ "5",
+                          grepl("S6", metabolite) ~ "6")
+  ) %>%
+  mutate(carbons = case_when(grepl("C4", metabolite) ~ "C4",
+                             grepl("C5", metabolite) ~ "C10",
+                             grepl("C6", metabolite) ~ "C10",
+                             grepl("C7", metabolite) ~ "C10",
+                             grepl("C8", metabolite) ~ "C10",
+                             grepl("C9", metabolite) ~ "C9",
+                           grepl("C10", metabolite) ~ "C10",
+                           grepl("C11", metabolite) ~ "C11",
+                           grepl("C12", metabolite) ~ "C12",
+                           grepl("C13", metabolite) ~ "C13",
+                           grepl("C14", metabolite) ~ "C14",
+                           grepl("C15", metabolite) ~ "C15",
+                           grepl("C16", metabolite) ~ "C16",
+                           grepl("C17", metabolite) ~ "C17",
+                           grepl("C18", metabolite) ~ "C18",
+                           grepl("C19", metabolite) ~ "C19",
+                           grepl("C20", metabolite) ~ "C20",
+                           grepl("C21", metabolite) ~ "C21",
+                           grepl("C22", metabolite) ~ "C22",
+                           grepl("C23", metabolite) ~ "C23",
+                           grepl("C24", metabolite) ~ "C24",
+                           grepl("C25", metabolite) ~ "C25",
+                           grepl("C26", metabolite) ~ "C26",
+                           grepl("C27", metabolite) ~ "C27")
+  )
+  
+# Add species info
+accession2species = read.delim("genotype2species.txt",header = T,stringsAsFactors = F)
+acylsugars.with.species = left_join(acylsugars.long.structure,accession2species,by="accession")
+
+#######################
+# Ordering of the plots
+#######################
+
+genotype_order_whiteflies = c("LA0716","PI127826","LYC4", "LA1777", 
+                              "PI134418", "LA1718", "LA1954","LA2695",
+                              "LA1401","LA0407","LA1364","LA4024", "LA2172", 
+                              "LA2133","LA1578","LA0735", "LA1278","MM","LA1840")
+
+genotype_order_thrips = c("LYC4","LA0407", "LA1777", "PI134418",
+                          "LA1401", "LA0716", "LA1278",  "LA2172", 
+                          "LA2695","LA0735","LA1718","LA2133","PI127826",
+                          "LA1578", "LA1954", "MM",  "LA1840", "LA4024","LA1364")
+
+########
+# plots #
+########
+
+# Set to accessions to WF survival order
+acylsugars.with.species$accession = factor(acylsugars.with.species$accession,
+                                              levels = genotype_order_whiteflies,
+                                              ordered = TRUE)
+
+#############################
+# Plot acyl-chain proportions #
+#############################
+
+# custom colours 
+cbp1 <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
+          "#0072B2", "#D55E00", "#CC79A7")
+
+p.acyl.proportion = 
+acylsugars.with.species %>%
+  dplyr::group_by(accession, backbone, tails, species, color) %>% 
+  summarise(mean_abundance = mean(abundance), 
+            n = n(), 
+            se = (sd(abundance)/sqrt(n))
+            ) %>%
+  ungroup %>%
+  ggplot(aes(x = accession, y = mean_abundance, fill = tails))+
+  geom_bar(position = "fill", stat = "identity")+
+  facet_wrap(~backbone, ncol = 1)+
+  theme_bw()+
+  theme(axis.text.x = element_text(color = "black", size = 10, angle = 45, hjust = 1))+
+  scale_fill_manual(values = cbp1)+
+  ylab("Proportion of sugar moiety")
+
+# Plot # acyl chains
+
+#p.backbones = 
+  acylsugars.with.species %>%
+  dplyr::group_by(accession, backbone) %>% 
+  summarise(mean_abundance = mean(abundance), 
+            n = n(), 
+            se = (sd(abundance)/sqrt(n))
+  ) %>%
+    mutate(cum_abundance = ave(mean_abundance, backbone, accession, FUN = cumsum)) %>%
+  ggplot(aes(x = accession, y = mean_abundance, fill = backbone))+
+  geom_bar(position = "stack", stat = "identity")+
+  geom_errorbar(aes(ymin = cum_abundance -se, ymax = cum_abundance+se))+
+  theme_bw()+
+  theme(axis.text.x = element_text(color = "black", size = 10, angle = 45, hjust = 1))+
+  scale_fill_manual(values = c("black","grey"))+
+  ylab("Proportion of sugar moiety")
+
+
+##
+
+
+# Plot # Carbon
+acylsugars.with.species %>%
+  dplyr::group_by(accession, backbone, carbons, species, color) %>% 
+  summarise(mean_abundance = mean(abundance), 
+            n = n(), 
+            se = (sd(abundance)/sqrt(n))
+  ) %>%
+  ggplot(aes(x = accession, y = mean_abundance, fill = carbons))+
+  geom_bar(position = "fill", stat = "identity")+
+  facet_wrap(~backbone, ncol = 1)+
+  theme_bw()+
+  scale_fill_manual(values = c25)
+
+########################
+# Average chain length #
+########################
+
+acylsugars.carbons =
+  acylsugars.with.species %>%
+  mutate(carbons_number = case_when(grepl("C4", carbons) ~ 4,
+                             grepl("C5", carbons) ~ 5,
+                             grepl("C6", carbons) ~ 6,
+                             grepl("C7", carbons) ~ 7,
+                             grepl("C8", carbons) ~ 8,
+                             grepl("C9", carbons) ~ 9,
+                             grepl("C10", carbons) ~ 10,
+                             grepl("C11", carbons) ~ 11,
+                             grepl("C12", carbons) ~ 12,
+                             grepl("C13", carbons) ~ 13,
+                             grepl("C14", carbons) ~ 14,
+                             grepl("C15", carbons) ~ 15,
+                             grepl("C16", carbons) ~ 16,
+                             grepl("C17", carbons) ~ 17,
+                             grepl("C18", carbons) ~ 18,
+                             grepl("C19", carbons) ~ 19,
+                             grepl("C20", carbons) ~ 20,
+                             grepl("C21", carbons) ~ 21,
+                             grepl("C22", carbons) ~ 22,
+                             grepl("C23", carbons) ~ 23,
+                             grepl("C24", carbons) ~ 24,
+                             grepl("C25", carbons) ~ 25,
+                             grepl("C26", carbons) ~ 26,
+                             grepl("C27", carbons) ~ 27)
+  )
+
+acylsugars.carbons$tails = as.numeric(acylsugars.carbons$tails)
+
+acylsugars.carbons <- acylsugars.carbons %>%
+  mutate(average_tail_lenght = carbons_number / tails)
+
+#plot
+acylsugars.carbons %>%
+  filter(average_tail_lenght < 15) %>%
+  dplyr::group_by(accession, backbone, average_tail_lenght, species, color) %>% 
+  summarise(mean_abundance = mean(abundance), 
+            n = n(), 
+            se = (sd(abundance)/sqrt(n))
+  ) %>%
+  ggplot(aes(x = accession, y = mean_abundance, fill = average_tail_lenght))+
+  geom_bar(position = "fill", stat = "identity")+
+  facet_wrap(~backbone, ncol = 1)+
+  scale_fill_gradient(low = "white", high = "black")
+
+#plot
+
+acylsugars.carbons %>%
+  dplyr::group_by(accession, backbone, tails, species, color) %>% 
+  summarise(mean_abundance = mean(abundance), 
+            n = n(), 
+            se = (sd(abundance)/sqrt(n))
+  ) %>%
+  ggplot(aes(x = accession, y = mean_abundance, fill = tails))+
+  geom_bar(position = "fill", stat = "identity")+
+  facet_wrap(~backbone, ncol = 1)+
+  scale_fill_gradient(low = "white", high = "black")
+
