f7bb65d1cdb39e7657b84f3a744e436a9cc4d807
Ruy Kortbeek <r.w.j.kortbeek@uva.nl>
2020-11-06 18:10:25 +0100

    changed order wf accessions

--- Figure_3/acylsugar_analytics/acylsugar_analysis.R
+++ Figure_3/acylsugar_analytics/acylsugar_analysis.R
@@ -70,10 +70,8 @@ acylsugars.with.species = left_join(acylsugars.long.structure,accession2species,
 # Ordering of the plots
 #######################
 
-genotype_order_whiteflies = c("LA0716","PI127826","LYC4", "LA1777", 
-                              "PI134418", "LA1718", "LA1954","LA2695",
-                              "LA1401","LA0407","LA1364","LA4024", "LA2172", 
-                              "LA2133","LA1578","LA0735", "LA1278","MM","LA1840")
+genotype_order_whiteflies = c("LA0716" ,  "PI127826", "LA1777" ,  "LYC4"   ,  "PI134418", "LA1718" ,  "LA1954" ,  "LA2695"  , "LA4024"  , "LA2172"  , "LA1401" , 
+                              "LA0407" ,  "LA1578"  , "LA1364" ,  "LA2133" ,  "MM"   ,    "LA1840"  , "LA0735" ,  "LA1278")
 
 genotype_order_thrips = c("LYC4","LA0407", "LA1777", "PI134418",
                           "LA1401", "LA0716", "LA1278",  "LA2172", 
