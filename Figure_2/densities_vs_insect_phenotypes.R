33ae356783c45a50926a6133effa6524c40a4ae9
Ruy Kortbeek <r.w.j.kortbeek@uva.nl>
2021-01-06 16:36:21 +0100

    Added linear models for NG-and summed trichomes

--- Figure_2/densities_vs_insect_phenotypes.R
+++ Figure_2/densities_vs_insect_phenotypes.R
@@ -103,6 +103,47 @@ p.thrips =
   my.theme+
   ylim(0, NA)
 
+###############################
+# plot total trichome density #
+###############################
+
+p.whitelfies.summed.density = 
+  density.pheno %>%
+  select(accession, species.x, type, density, leaf.side, wf_relative_survival) %>%
+  dplyr::group_by(accession, species.x, type, wf_relative_survival) %>%
+  dplyr::summarise(avg.density = mean(density)) %>%
+  dplyr::group_by(accession, species.x, wf_relative_survival) %>%
+  dplyr::summarise(sum.types = sum(avg.density)) %>%
+  ggplot() +
+  geom_point(aes(x = wf_relative_survival, y =sum.types),fill="grey",color="black",shape=21,size=2) +
+  theme_bw() +
+  geom_label_repel(aes(x = wf_relative_survival, y=sum.types,label=accession, fill=species.x),
+                   size = 2,
+                   label.size = 0.05,
+                   label.padding = 0.1,
+                   show.legend = FALSE) +
+  geom_smooth(aes(x=wf_relative_survival, y = sum.types), method = "lm", alpha = 0.2, color = "black", size = 0.2)+
+  labs(x = "Relative whitefly survival",y = "Trichome density (trichomes / mm2 leaf)") +
+  my.theme+
+  ylim(0, NA)
+
+p.thrips.summed.density = 
+  density.pheno %>% filter(leaf.side == "adaxial") %>%
+  dplyr::group_by(accession, species.x, thrips_relative_survival) %>%
+  dplyr::summarise(sum.types = sum(density)) %>%
+  ggplot() +
+  geom_point(aes(x = thrips_relative_survival, y = sum.types),fill="grey",color="black",shape=21,size=2) +
+  theme_bw() +
+  geom_label_repel(aes(x = thrips_relative_survival, y=sum.types,label=accession, fill=species.x),
+                   size = 2,
+                   label.size = 0.05,
+                   label.padding = 0.1,
+                   show.legend = FALSE) +
+  geom_smooth(aes(x =thrips_relative_survival, y = sum.types), method = "lm", alpha = 0.2, color = "black", size = 0.2)+
+  labs(x = "Relative thrips survival",y = "Trichome density (trichomes / mm2 leaf)") +
+  my.theme+
+  ylim(0, NA)
+
 
 #################
 # Linear models #
@@ -124,8 +165,18 @@ lm.wf.typeVI = summary(
        dplyr::group_by(accession, species.x, type, wf_relative_survival) %>%
        dplyr::summarise(avg.density = mean(density))  %>% filter(., type == "Type VI"),
           wf_relative_survival ~ avg.density)
+
 )
 
+# whitefly vs (relative) NG trichome density
+lm.wf.NG = summary(
+  lm(data =  density.pheno %>%
+       select(accession, species.x, type, density, leaf.side, wf_relative_survival) %>%
+       dplyr::group_by(accession, species.x, type, wf_relative_survival) %>%
+       dplyr::summarise(avg.density = mean(density))  %>% filter(., type == "Non glandular"),
+     wf_relative_survival ~ avg.density)
+  
+)
 # thrips vs (relative) type I/IV trichome density
 lm.thrips.typeIIV = summary(
   lm(data = density.pheno %>% filter(., type == "Type I/IV") %>% filter(leaf.side == "adaxial"),
@@ -138,11 +189,41 @@ lm.thrips.typeVI = summary(
                   thrips_relative_survival ~ density)
   )
 
+# thrips vs (relative) NG trichome density
+lm.thrips.NG = summary(
+  lm(data = density.pheno %>% filter(., type == "Non glandular") %>% filter(leaf.side == "adaxial"),
+     thrips_relative_survival ~ density)
+)
+
+
+lm.wf.sum.types = summary( 
+  lm(data = density.pheno %>%
+       select(accession, species.x, type, density, leaf.side, wf_relative_survival) %>%
+       dplyr::group_by(accession, species.x, type, wf_relative_survival) %>%
+       dplyr::summarise(avg.density = mean(density)) %>% 
+       dplyr::group_by(accession, species.x, wf_relative_survival) %>%
+       dplyr::summarise(sum.types = sum(avg.density)),
+     wf_relative_survival~ sum.types)
+)
+
+lm.thrips.sum.types = summary(
+  lm(data = density.pheno %>% filter(., type == "Type VI") %>% 
+       filter(leaf.side == "adaxial") %>%
+       dplyr::group_by(accession, species.x, thrips_relative_survival) %>%
+       dplyr::summarise(sum.types = sum(density)),
+     thrips_relative_survival ~ sum.types)
+)
+
 
 capture.output(lm.wf.typeIIV, file = "Figure_2/Linear models/linear_model_wf_typeI_IV")
 capture.output(lm.wf.typeVI, file = "Figure_2/Linear models/linear_model_wf_type_VI")
+capture.output(lm.wf.NG, file = "Figure_2/Linear models/linear_model_wf_Non-glandular")
 capture.output(lm.thrips.typeIIV, file = "Figure_2/Linear models/linear_model_thrips_typeI_IV")
 capture.output(lm.thrips.typeVI, file = "Figure_2/Linear models/linear_model_thrips_type_VI")
+capture.output(lm.thrips.NG, file = "Figure_2/Linear models/linear_model_thrips_Non-glandular")
+
+capture.output(lm.wf.sum.types, file = "Figure_2/Linear models/linear_model_wf_summed_types")
+capture.output(lm.thrips.sum.types, file = "Figure_2/Linear models/linear_model_thrips_summed_types")
 
 
 
@@ -151,6 +232,10 @@ capture.output(lm.thrips.typeVI, file = "Figure_2/Linear models/linear_model_thr
 ################
 ggsave(filename = file.path("Figure_2/whitefly_toxicity_vs_trichome_densities.pdf"),plot = p.whitelfies, width = 9,height = 21, units = "cm")
 ggsave(filename = file.path("Figure_2/thrips_toxicity_vs_trichome_densities.pdf"),plot = p.thrips, width = 9,height = 21, units = "cm")
+
+
+ggsave(filename = file.path("Figure_2/thrips_toxicity_vs_summed_trichome_densities.pdf"),plot = p.thrips.summed.density, width = 9,height = 7, units = "cm")
+ggsave(filename = file.path("Figure_2/whitefly_toxicity_vs_summed_trichome_densities.pdf"),plot = p.thrips.summed.density, width = 9,height = 7, units = "cm")
 
 #################
 # session info
